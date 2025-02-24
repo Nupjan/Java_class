9418c5ae94486efd6a6c23a4f98100a25e09240c
Nupjan <nupjan.5koirala@gmail.com>
2025-02-25 10:55:37 +1100

    Using Escape Sequences

--- New_project.java
+++ New_project.java
@@ -2,6 +2,15 @@ public class New_project {
     public static void main(String[] args) {
         System.out.println("This is a new project.");
         System.out.println("Hello World!");
+        // Start of a new language
 
+        System.out.println("S.N. \tName \t\"Surname\"");
+        System.out.println("=======================\b================\r==");
+        System.out.println("1. \tHarry \tStyles");
+        System.out.println("2. \tToki \tHiro");
+        System.out.println("3. \tRajesh \tHamal");
+        System.out.println("4. \tTank \tWing");
+        System.out.println("5. \tNori \tTsubasa");
+        // Using Escape sequences
     }
 }
