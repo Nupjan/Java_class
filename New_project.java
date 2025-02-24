0c031b60c6ffc764aabf767d4b95a7d65b0cb8e9
Nupjan <nupjan.5koirala@gmail.com>
2025-02-25 09:41:24 +1100

    Start

---
+++ New_project.java
@@ -0,0 +1,7 @@
+public class New_project {
+    public static void main(String[] args) {
+        System.out.println("This is a new project.");
+        System.out.println("Hello World!");
+
+    }
+}
