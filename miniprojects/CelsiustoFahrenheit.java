3e8548aa93a76f470880f5194fbd20000cc36a32
Nupjan <nupjan.5koirala@gmail.com>
2025-02-27 11:27:09 +1100

    test

---
+++ miniprojects/CelsiustoFahrenheit.java
@@ -0,0 +1,10 @@
+
+
+        int celsius = 28;
+        int fahrenheit = 32 + (9 * celsius) / 5;
+        System.out.print("Celcius: ");
+        System.out.println(celsius);
+
+        System.out.print("equals Fahrenheit temperature: " + fahrenheit);
+    }
+}
